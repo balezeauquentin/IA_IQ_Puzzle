0cfa6ecc56232b78303abcdcf4e24cdb6b97be47
estouan <120207575+estouan@users.noreply.github.com>
2023-10-19 11:32:08 +0200

    ca marche

--- brutforce.py
+++ brutforce.py
@@ -6,11 +6,32 @@ import interface
 #         for j in range(len(table.board[0])):
 #             if table.board[i][j]==0:
 #                 if i==0 or table.board[i-1][j]!=0 and i==
+#
+def case_isolee(plateau, ligne, colonne):
+    # Les coordonnées des cases voisines
+    voisins = [(ligne - 1, colonne), (ligne + 1, colonne), (ligne, colonne - 1), (ligne, colonne + 1)]
+
+    # Vérifie si **toutes** les cases voisines sont différentes de zéro
+    for voisin_ligne, voisin_colonne in voisins:
+        if (
+            0 <= voisin_ligne < len(plateau) and  # Vérifie si les coordonnées sont dans les limites du plateau
+            0 <= voisin_colonne < len(plateau[0]) and
+            plateau[voisin_ligne][voisin_colonne] == 0
+        ):
+            return False  # Il y a au moins une case vide à proximité, la case n'est pas isolée
+
+    return True  # Toutes les cases voisines sont différentes de zéro, la case est isolée
+def table_a_des_cases_isolees(plateau):
+    for i in range(len(plateau)):
+        for j in range(len(plateau[0])):
+            if plateau[i][j] == 0 and case_isolee(plateau, i, j):
+                return False
+    return True
 
 
-def brutforce(affichage, used_pieces,ayaya, position=(0, 0)):
+def brutforce(affichage, used_pieces,table, position=(0, 0)):
     affichage.update()
-    table = affichage.plateau
+
     if 0 not in used_pieces:
         # Toutes les pièces ont été utilisées, nous avons une solution.
         print("Solution trouvée:")
@@ -18,6 +39,8 @@ def brutforce(affichage, used_pieces,ayaya, position=(0, 0)):
         plateau_solution.board = table
         plateau_solution.printBoard()
         affichage.update()
+        return
+    if not table_a_des_cases_isolees(table):
         return
 
     i, j = position
@@ -34,7 +57,9 @@ def brutforce(affichage, used_pieces,ayaya, position=(0, 0)):
 
     if i == len(table):
         # Toutes les cases ont été remplies, mais nous n'avons pas encore de solution.
+        print("Pas de solution trouvée.")
         return
+
     temp_table = jeu.Board(len(table), len(table[0]))
     temp_table.board = [row[:] for row in table]
     for piece_id in range(1, 13):  # Mise à jour pour 12 pièces
@@ -49,16 +74,18 @@ def brutforce(affichage, used_pieces,ayaya, position=(0, 0)):
                     next_position = (i, j + 1)
                     if next_position[1] == len(table[0]):
                         next_position = (i + 1, 0)
-                    used_pieces[piece_id - 1] = 1
+
                     updated_used_pieces = used_pieces[:]
-                    ayaya=ayaya+piece_id
-                    # temp_table.afficher_tableau_console()
-                    affichage.plateau = temp_table
-                    print(ayaya)
-                    brutforce(affichage, updated_used_pieces,ayaya, next_position)
-                    affichage.remove(piece_id)
+                    updated_used_pieces[piece_id - 1] = 1
+
+                    affichage.board.board=temp_table.board
+                    brutforce(affichage, updated_used_pieces,temp_table, next_position)
+                    affichage.removeShape(piece_id)
+                    used_pieces[piece_id - 1] = 0
+                    temp_table.board = [row[:] for row in table]
+
                 current_piece.turnClockwise()  # Renomme la fonction pour éviter le conflit de noms
 
 if __name__ == "__main__":
     a = interface.Interface()
-    brutforce(a, [0 for _ in range(12)],0)
+    brutforce(a, [0 for _ in range(12)],a.board)
