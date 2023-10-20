ccc4fd72e93a7eec6c00976c62bc48b4c0af451a
Olivier <guillaume.py20@gmail.com>
2023-10-20 16:57:11 +0200

    Ajout de la fonction qui permet de savoir quand on a gagné

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import brutforce
 
 
 
-pieces = range(1, 13) # Liste des pièces de 1 à 12
+pieces = [i for i in range(1,13)] # Liste des pièces de 1 à 12
 affichage=interface.Interface()
 brutforce.brutforce(affichage, pieces, [0 for _ in range(12)])
