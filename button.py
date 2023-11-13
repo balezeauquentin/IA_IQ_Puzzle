bd10332de2c947a518afe0315f99804efa02476f
Olivier <guillaume.py20@gmail.com>
2023-11-13 15:44:52 +0100

    Ajout d'un menu principal basique

--- button.py
+++ button.py
@@ -6,7 +6,7 @@ class Button:
 
     def __init__(self, pos:tuple[int,int],
                  size:tuple[int,int],
-                 screen:pg.Surface,
+                 surface:pg.Surface,
                  bg_color = (0,0,0),
                  text:str = None,
                  text_color = (255,255,255),
@@ -19,7 +19,7 @@ class Button:
                  callbak_args = ()):
         
         self.size = size
-        self.screen = screen
+        self.surface = surface
         self.bg_color = bg_color
         self.text = text
         self.text_color = text_color
@@ -28,9 +28,9 @@ class Button:
         self.callback = callback
 
         if (pos[0] == self.CENTERED):
-            pos = (self.screen.get_width()//2-size[0]//2, pos[1])
+            pos = (self.surface.get_width()//2-size[0]//2, pos[1])
         if (pos[1] == self.CENTERED):
-            pos = (pos[0], self.screen.get_height()//2-size[1]/2)
+            pos = (pos[0], self.surface.get_height()//2-size[1]/2)
 
         self.rect = pg.Rect(pos, size)
         self.border = pg.Rect((pos[0]-border_size,pos[1]-border_size), (size[0]+border_size, size[1]+border_size))
@@ -46,19 +46,19 @@ class Button:
         if self.border_color != None:
             border_draw = pg.Surface((self.size[0]+self.border_size*2, self.size[1]+self.border_size*2))
             border_draw.fill(self.border_color)
-            self.screen.blit(border_draw, (self.pos[0]-self.border_size, self.pos[1]-self.border_size))
+            self.surface.blit(border_draw, (self.pos[0]-self.border_size, self.pos[1]-self.border_size))
 
         # Draws the button
         button_draw = pg.Surface(self.size)
         button_draw.fill(self.bg_color)
-        self.screen.blit(button_draw, self.pos)
+        self.surface.blit(button_draw, self.pos)
 
         # Draws the button text
         if self.text != None:
             text_draw = self.font.render(self.text, self.font_alliasing, self.text_color)
             text_draw_rect = text_draw.get_rect()
             text_draw_rect.center = self.rect.center
-            self.screen.blit(text_draw, text_draw_rect)
+            self.surface.blit(text_draw, text_draw_rect)
     
     def update(self) -> None:
         if (pg.mouse.get_pressed()[0] and self.isHovered() and not self.prev_state):
